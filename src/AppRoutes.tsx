fbc42978d14586b7e3c26905d3227ff846623480
Yunjiman <yunjiman1992@gmail.com>
2022-11-18 12:55:52 +0900

    add raid info & init

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -4,6 +4,7 @@ import Auth from './Auth';
 import CharSettingContent from './components/CharSettingContent/CharSettingContent';
 import MainLayout from './components/Layout/MainLayout';
 import MainContent from './components/MainContent/MainContent';
+import RaidInfo from './components/RaidInfo/RaidInfo';
 import ROUTE_PATH from './enum/route.enum';
 
 const AppRoutes = () => {
@@ -20,7 +21,7 @@ const AppRoutes = () => {
         >
           <Route path={ROUTE_PATH.MAIN} element={<MainContent />} />
           <Route path={ROUTE_PATH.CHAR_SETTING} element={<CharSettingContent />} />
-          <Route path={ROUTE_PATH.RAID_INFO} element={<MainContent />} />
+          <Route path={ROUTE_PATH.RAID_INFO} element={<RaidInfo />} />
           <Route path='*' element={<Navigate to={`/${ROUTE_PATH.MAIN}`} replace />} />
         </Route>
       </Routes>
---
+++ src/components/RaidInfo/RaidInfo.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RaidInfo = () => {
+  return (
+    <div className='p-3'>
+      <img src='http://www.lochek.site/av1dog.jpg' alt='' />
+    </div>
+  );
+};
+
+export default RaidInfo;
